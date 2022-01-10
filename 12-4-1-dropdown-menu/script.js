e7def6e0516eee4fbfce7ec08db4361dbe263e09
Kim J Kubik <kkubik7@hotmail.com>
2022-01-09 19:16:55 -0600

    thru 12.4.1-dropdown menu

---
+++ 12-4-1-dropdown-menu/script.js
@@ -0,0 +1,25 @@
+// create an event listener
+d3.selectAll("body").on("change", updatePage);
+
+
+
+// performed when HTML body is changed
+function updatePage() {
+    var dropdownMenu = d3.selectAll("#selectOption").node();
+    var dropdownMenuID = dropdownMenu.id;
+    var selectedOption = dropdownMenu.value;
+    //var Text = dropdownMenu.node().value;
+    //var selectedText = dropdownMenu.node().value
+
+    console.log(dropdownMenu);
+    console.log(dropdownMenuID);
+    //console.log(Text);
+
+    //console.log(selectText);
+    //console.log(d3.selectAll('#menu').selection.text());
+    console.log(d3.selectAll("#selectOption").value);
+};
+
+console.log(d3.selectAll("#menu").node().id);
+
+//console.log(d3.selectedOption);
